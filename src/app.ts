cb7fd00ad0c79ce464ce1ee86f33a23186c70fb6
elliot-p <park.hanjun00@gmail.com>
2020-11-08 20:15:18 +0900

    :art: ADD SocketIO

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import { createServer } from 'http';
+import * as SocketIO from 'socket.io';
 import createError from 'http-errors';
 
 import initLoaders from './loaders';
 import indexRouter from './routes';
 import usersRouter from './routes/users';
+import addSocketListener from './socketIO';
 
 const env: string = process.env.NODE_ENV || 'development';
 const port: number = Number(process.env.PORT) || 8888;
@@ -30,9 +32,18 @@ const startServer = async () => {
     });
 
     const server = createServer(app);
+
     server.listen(port, () => {
         console.log(`> Ready on port ${port} [${env}]`);
     });
+
+    /**
+     * socket io
+     */
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const io: SocketIO.Server = require('socket.io')(server);
+    addSocketListener(io);
 };
 
 startServer();
