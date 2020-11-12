b585aee629bc95fad0b25a09a84ff63aecb60c39
Park hanjun <park.hanjun00@gmail.com>
2020-11-12 21:42:40 +0900

    :art: swagger 모델 적용

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,11 +6,42 @@ const router: express.Router = express.Router();
 
 /**
  * @swagger
+ * definitions:
+ *   User:
+ *     type: "object"
+ *     required:
+ *       - "email"
+ *       - "name"
+ *     properties:
+ *       email:
+ *         type: "string"
+ *         format: "email"
+ *       name:
+ *         type: "string"
+ *         example: "hong-gil-dong"
+ *       nickname:
+ *         type: "string"
+ *         example: "hong-gil-dong"
+ */
+
+/**
+ * @swagger
  * /user/:userId:
  *   get:
  *     summary: "Returns User"
  *     tags:
  *       - "User"
+ *     consumes:
+ *       "application/json"
+ *     produces:
+ *       "application/json"
+ *     parameters:
+ *       - in: "body"
+ *         name: "body"
+ *         description: "User object that needs to be added to the store"
+ *         required: true
+ *         schema:
+ *           $ref: "#/definitions/User"
  *     responses:
  *       "200":
  *         description: "User Data"
